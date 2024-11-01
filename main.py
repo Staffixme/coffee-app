e47a4aef7fb4993921b6a8dba0cc12cb2a980220
wecto <a.trunov1000@gmail.com>
2024-11-01 19:10:14 +0900

    commit main.py edited

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
-from PyQt6.QtWidgets import QWidget, QTableWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QTableWidget, QApplication, QTableWidgetItem
 from PyQt6 import uic
 
 import sqlite3
+
 
 class CoffeeApp(QWidget):
     def __init__(self):
@@ -12,9 +13,9 @@ class CoffeeApp(QWidget):
         self.initTable()
 
     def initTable(self):
-
         self.tableWidget.setColumnCount(6)
-        self.tableWidget.setHorizontalHeaderLabels(["Сорт", "Обжарка", "Молотый/В зернах", "Вкус", "Цена", "Объем упаковки"])
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["Сорт", "Обжарка", "Молотый/В зернах", "Вкус", "Цена", "Объем упаковки"])
 
         with sqlite3.connect("coffee.sqlite") as db:
             cur = db.cursor()
@@ -24,14 +25,14 @@ class CoffeeApp(QWidget):
 
         self.tableWidget.setRowCount(len(data))
 
-        for i in range(len(data) - 1):
-            self.tableWidget.setItemLabel(i, 0, data[1])
-            self.tableWidget.setItemLabel(i, 1, data[2])
-            self.tableWidget.setItemLabel(i, 2, data[3])
-            self.tableWidget.setItemLabel(i, 3, data[4])
-            self.tableWidget.setItemLabel(i, 4, data[5])
-            self.tableWidget.setItemLabel(i, 5, data[6])
-
+        for i in range(len(data)):
+            self.tableWidget.setRowHeight(i, 75)
+            self.tableWidget.setItem(i, 0, QTableWidgetItem(data[i][1]))
+            self.tableWidget.setItem(i, 1, QTableWidgetItem(data[i][2]))
+            self.tableWidget.setItem(i, 2, QTableWidgetItem(data[i][3]))
+            self.tableWidget.setItem(i, 3, QTableWidgetItem(data[i][4]))
+            self.tableWidget.setItem(i, 4, QTableWidgetItem(str(data[i][5])))
+            self.tableWidget.setItem(i, 5, QTableWidgetItem(data[i][6]))
 
 
 if __name__ == "__main__":
